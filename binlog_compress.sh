48ff1fde6ee8b9c09a3c2fec839b9b418e9ccf6a
Yunus Shaikh <45660881+yunushaikh@users.noreply.github.com>
2024-02-11 00:33:57 -0500

    Update binlog_compress.sh

--- binlog_compress.sh
+++ binlog_compress.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 timeinmin=300
 destdir=/var/lib/mysql/
 binlogname=v2dbmain-bin
@@ -6,13 +6,14 @@ excludefile=v2dbmain-bin.index
 retention=5
 timestamp=`date '+%Y-%m-%d %H:%M:%S'`
 
-find $destdir -name '$binlogname.*' -type f ! -name $excludefile -mmin +$timeinmin -exec gzip {} \;
+
+find $destdir -name "$binlogname.*" -type f ! -name $excludefile -mmin +$timeinmin -exec gzip {} \;
 if [ $? -eq 0 ]; then
     echo "compression completed for binlog at $timestamp "
 else
     echo "Command failed for compression at $timestamp"
 fi
-find $destdir -name '*.gzip' -type f -mtime +$retention -exec rm -f {} \;
+find $destdir -name "*.gz" -type f -mtime +$retention -exec rm -f {} \;
 if [ $? -eq 0 ]; then
     echo "Deletion for binary completed at $timestamp "
 else
