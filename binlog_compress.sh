52df2cd9835cebb9e390953183c7deef9e4e013a
Yunus Shaikh <45660881+yunushaikh@users.noreply.github.com>
2024-02-11 00:04:34 -0500

    Create binlog_compress.sh

---
+++ binlog_compress.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+timeinmin=300
+destdir=/var/lib/mysql/
+binlogname=v2dbmain-bin
+excludefile=v2dbmain-bin.index
+retention=5
+timestamp=`date '+%Y-%m-%d'`
+
+find $destdir -name '$binlogname.*' -type f ! -name $excludefile -mmin +$timeinmin -exec gzip {} \;
+if [ $? -eq 0 ]; then
+    echo "compression completed for binlog at $timestamp "
+else
+    echo "Command failed for compression at $timestamp"
+fi
+find $destdir -name '*.gzip' -type f -mtime +$retention -exec rm -f {} \;
+if [ $? -eq 0 ]; then
+    echo "Deletion for binary completed at $timestamp "
+else
+    echo "Command failed for deletion at $timestamp"
+fi
